1474c9df7c855649edc0899a0bf37cb256d4fe6f
Felipe Milana <felipe.milana97@gmail.com>
2020-10-27 17:26:49 -0300

    Project created

---
+++ src/main/java/com/javaudemy/webService_springBoot_jpa/WebServiceSpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.javaudemy.webService_springBoot_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceSpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceSpringBootJpaApplication.class, args);
+	}
+
+}
