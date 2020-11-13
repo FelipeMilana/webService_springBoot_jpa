de0eebcaa1ee7eabfa5ecac2a113069ebc50eb3a
Felipe Milana <felipe.milana97@gmail.com>
2020-11-13 16:49:23 -0300

    User insert, delete and update

--- src/main/java/com/javaudemy/webService_springBoot_jpa/resources/UserResource.java
+++ src/main/java/com/javaudemy/webService_springBoot_jpa/resources/UserResource.java
@@ -1,13 +1,19 @@
 package com.javaudemy.webService_springBoot_jpa.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.javaudemy.webService_springBoot_jpa.entities.User;
 import com.javaudemy.webService_springBoot_jpa.services.UserService;
@@ -19,6 +25,9 @@ public class UserResource {
 	@Autowired
 	private UserService userService;
 	
+	/*EndPoints with @Get annotations, are used to 
+	 * return or recover something from database
+	 */
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = userService.findAll();	
@@ -28,6 +37,34 @@ public class UserResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	/*EndPoints with @Post annotation, are used to insert
+	 * something on Database
+	 */
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	/*EndPoints with @Delete annotation, are used to delete
+	 * something on Database
+	 */
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	/*EndPoints with @Put annotation, are used to update
+	 * something on Database
+	 */
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = userService.update(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/javaudemy/webService_springBoot_jpa/services/UserService.java
+++ src/main/java/com/javaudemy/webService_springBoot_jpa/services/UserService.java
@@ -23,4 +23,32 @@ public class UserService {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
+	
+	/*Save method, already return the object that will be 
+	 * saved on DataBase
+	 */
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
+	
+	/*We use an id and an object as an argument, because 
+	 * with id, we identify who are this user, and with the
+	 * object, we have the new data about this user. The getOne just
+	 * prepare not recover the user from database.
+	 */
+	public User update(Long id, User obj) {
+		User entity = userRepository.getOne(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
